e6bfcf0c15e8f1d212af12bd85ba19dc97c5a46f
Mohit Srivastava <mohitsriv.2006@gmail.com>
2025-08-07 11:28:59 +0530

    fixing dummy api

--- shell-app/app/dashboard/actions.ts
+++ shell-app/app/dashboard/actions.ts
@@ -1,6 +1,75 @@
-import {fetcher} from '@/api/fetcher'
-
 export const getConfig = async () => {
-  const resp = await fetcher(`${process.env.NEXT_PUBLIC_MOCK_API_URL}`)
-  return resp
-}
+  // const resp = await fetcher(`${process.env.NEXT_PUBLIC_MOCK_API_URL}`);
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        headerConfig: [
+          {
+            id: "clothing",
+            path: "/shopping/clothing",
+            title: "Clothing",
+            active: false
+          },
+          {
+            id: "electronics",
+            path: "/shopping/electronics",
+            title: "Electronics",
+            active: false,
+            baseUrl: ""
+          },
+          {
+            id: "mobiles",
+            path: "/shopping/mobiles",
+            title: "Mobiles",
+            active: false,
+            baseUrl: ""
+          }
+        ],
+        leftNavConfig: [
+          {
+            id: "profile",
+            path: "/profile",
+            title: "Profile",
+            active: false,
+            baseUrl: ""
+          },
+          {
+            id: "cart",
+            path: "/cart",
+            title: "Cart",
+            active: false,
+            baseUrl:
+              "https://dynamic-microfrontends-ui.vercel.app/cart-app/cartApp.js",
+            webComponent: "cart-app"
+          },
+          {
+            id: "orders",
+            path: "/orders",
+            title: "Orders",
+            active: false,
+            baseUrl: ""
+          }
+        ],
+        secondaryConfig: [
+          {
+            id: "checkout",
+            path: "/cart/checkout",
+            title: "Checkout",
+            active: false,
+            baseUrl:
+              "https://dynamic-microfrontends-ui.vercel.app/checkout-app/checkoutApp.js",
+            webComponent: "checkout-app"
+          },
+          {
+            id: "payment",
+            path: "/orders/payment",
+            title: "Payment",
+            active: false,
+            baseUrl: ""
+          }
+        ]
+      });
+    }, 2000);
+  });
+};
--- shell-app/app/stores/appConfigStore.ts
+++ shell-app/app/stores/appConfigStore.ts
@@ -1,12 +1,12 @@
-import {atom, computed} from 'nanostores'
-import {AppConfigType, MicrofrontendConfigType} from '@/types'
-import {EMPTY_APP_CONFIG} from '@/dashboard/constants'
-import {getConfig} from '@/dashboard/actions'
+import { atom, computed } from "nanostores";
+import { AppConfigType, MicrofrontendConfigType } from "@/types";
+import { EMPTY_APP_CONFIG } from "@/dashboard/constants";
+import { getConfig } from "@/dashboard/actions";
 
-export const appConfig = atom<AppConfigType>(EMPTY_APP_CONFIG)
-export const headerConfig = atom<MicrofrontendConfigType[]>([])
-export const leftNavConfig = atom<MicrofrontendConfigType[]>([])
-export const secondaryConfig = atom<MicrofrontendConfigType[]>([])
+export const appConfig = atom<AppConfigType>(EMPTY_APP_CONFIG);
+export const headerConfig = atom<MicrofrontendConfigType[]>([]);
+export const leftNavConfig = atom<MicrofrontendConfigType[]>([]);
+export const secondaryConfig = atom<MicrofrontendConfigType[]>([]);
 export const allConfigs = computed(
   [headerConfig, leftNavConfig, secondaryConfig],
   () => {
@@ -14,42 +14,42 @@ export const allConfigs = computed(
       ...headerConfig.get(),
       ...leftNavConfig.get(),
       ...secondaryConfig.get()
-    ]
+    ];
   }
-)
-export const showUploadConfig = atom<boolean>(false)
-export const uploadedConfig = atom<string>('')
+);
+export const showUploadConfig = atom<boolean>(false);
+export const uploadedConfig = atom<string>("");
 
 export const fetchAppConfig = async () => {
-  const lsConfig = window.localStorage.getItem('appConfig')
-  let appConfigData: AppConfigType = EMPTY_APP_CONFIG
+  const lsConfig = window.localStorage.getItem("appConfig");
+  let appConfigData: AppConfigType = EMPTY_APP_CONFIG;
 
   // fetch config from server if no config in local storage
   if (!lsConfig) {
-    appConfigData = await getConfig()
-    window.localStorage.setItem('appConfig', JSON.stringify(appConfigData))
+    appConfigData = (await getConfig()) as AppConfigType;
+    window.localStorage.setItem("appConfig", JSON.stringify(appConfigData));
   } else {
-    appConfigData = JSON.parse(lsConfig)
+    appConfigData = JSON.parse(lsConfig) as AppConfigType;
   }
 
-  appConfig.set(appConfigData)
-  headerConfig.set(appConfigData?.headerConfig ?? [])
-  leftNavConfig.set(appConfigData?.leftNavConfig ?? [])
-  secondaryConfig.set(appConfigData?.secondaryConfig ?? [])
-  uploadedConfig.set(JSON.stringify(appConfigData))
-}
+  appConfig.set(appConfigData);
+  headerConfig.set(appConfigData?.headerConfig ?? []);
+  leftNavConfig.set(appConfigData?.leftNavConfig ?? []);
+  secondaryConfig.set(appConfigData?.secondaryConfig ?? []);
+  uploadedConfig.set(JSON.stringify(appConfigData));
+};
 
 export const uploadConfig = async () => {
-  if (uploadedConfig.get() === '') {
-    alert('uploadedConfig is empty')
-    return
+  if (uploadedConfig.get() === "") {
+    alert("uploadedConfig is empty");
+    return;
   }
-  const appConfigData: AppConfigType = JSON.parse(uploadedConfig.get())
-  window.localStorage.setItem('appConfig', JSON.stringify(appConfigData))
+  const appConfigData: AppConfigType = JSON.parse(uploadedConfig.get());
+  window.localStorage.setItem("appConfig", JSON.stringify(appConfigData));
 
-  appConfig.set(appConfigData)
-  headerConfig.set(appConfigData?.headerConfig ?? [])
-  leftNavConfig.set(appConfigData?.leftNavConfig ?? [])
-  secondaryConfig.set(appConfigData?.secondaryConfig ?? [])
-  showUploadConfig.set(false)
-}
+  appConfig.set(appConfigData);
+  headerConfig.set(appConfigData?.headerConfig ?? []);
+  leftNavConfig.set(appConfigData?.leftNavConfig ?? []);
+  secondaryConfig.set(appConfigData?.secondaryConfig ?? []);
+  showUploadConfig.set(false);
+};
